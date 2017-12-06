b7ece83fe1adfa2c437e4337984e16e79a4e0c25
helloyesterday <yesterday.young@gmail.com>
2017-12-06 14:28:55 +0100

    Change the ShiftBias to the normal version

--- src/bias/ReweightBias.cpp
+++ src/bias/ReweightBias.cpp
@@ -1,5 +1,5 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2011-2016 The plumed team
+   Copyright (c) 2016,2017 The plumed team
    (see the PEOPLE file at the root of the distribution for a list of names)
 
    See http://www.plumed.org for more information.
@@ -22,31 +22,31 @@
 #include "ReweightBase.h"
 #include "core/ActionRegister.h"
 
-//+PLUMEDOC REWEIGHTING REWEIGHT_BIAS_SHIFT
+//+PLUMEDOC REWEIGHTING REWEIGHT_BIAS
 /*
 Calculate weights for ensemble averages that negate the effect the bias has on the region of phase space explored
 
-If a static or pseudo-static bias \f$V(x,t')\f$ is acting on 
+If a static or pseudo-static bias \f$V(x,t')\f$ is acting on
 the system we can remove the bias and get the unbiased probability distribution using:
 
 \f[
-\langle P(s',t) \rangle = \frac{ \sum_{t'}^t \delta( s(x) - s' ) \exp\left( +\frac{V(x,t')}{k_B T} \right) }{ \sum_t'^t \exp\left( +\frac{V(x,t')}{k_B T} \right) } 
+\langle P(s',t) \rangle = \frac{ \sum_{t'}^t \delta( s(x) - s' ) \exp\left( +\frac{V(x,t')}{k_B T} \right) }{ \sum_t'^t \exp\left( +\frac{V(x,t')}{k_B T} \right) }
 \f]
 
-The weights calculated by this action are equal to \f$\exp\left( +\frac{V(x,t')}{k_B T} \right)\f$ these weights can then be used in any action 
-that computes ensemble averages.  For example this action can be used in tandem with \ref HISTOGRAM or \ref AVERAGE. 
+The weights calculated by this action are equal to \f$\exp\left( +\frac{V(x,t')}{k_B T} \right)\f$ these weights can then be used in any action
+that computes ensemble averages.  For example this action can be used in tandem with \ref HISTOGRAM or \ref AVERAGE.
 
 \par Examples
 
-In the following example there is a fixed restraint on the distance between atoms 1 and 2.  Clearly, this 
+In the following example there is a fixed restraint on the distance between atoms 1 and 2.  Clearly, this
 restraint will have an effect on the region of phase space that will be sampled when an MD simulation is
 run using this variable.  Consequently, when the histogram as a function of the distance, \f$x\f$, is accumulated,
 we use reweighting into order to discount the effect of the bias from our final histogram.
 
-\verbatim 
+\plumedfile
 x: DISTANCE ATOMS=1,2
 RESTRAINT ARG=x SLOPE=1.0 AT=0.0
-as: REWEIGHT_BIAS_SHIFT TEMP=300
+bias: REWEIGHT_BIAS TEMP=300
 
 HISTOGRAM ...
   ARG=x
@@ -59,29 +59,7 @@ HISTOGRAM ...
 ... HISTOGRAM
 
 DUMPGRID GRID=hB FILE=histoB STRIDE=1 FMT=%8.4f
-\endverbatim
-
-Sometimes, when the value of bias potential are too large or too small, the calculation of reweighting factor at each frame may be beyond the data range.
-In this situation, a constant value is needed to shift the value of bias potential. For example, the values of bias potential during the biased simulation
-are almost larger than 2000, we can add a shift value -2000 (SHIFT=-2000) to the bias at reweighting:
-
-\verbatim 
-x: DISTANCE ATOMS=1,2
-RESTRAINT ARG=x SLOPE=1.0 AT=0.0
-as: REWEIGHT_BIAS_SHIFT TEMP=300 SHIFT=-2000
-
-HISTOGRAM ...
-  ARG=x
-  GRID_MIN=0.0
-  GRID_MAX=3.0
-  GRID_BIN=100
-  BANDWIDTH=0.1
-  LOGWEIGHTS=bias
-  LABEL=hB
-... HISTOGRAM
-
-DUMPGRID GRID=hB FILE=histoB STRIDE=1 FMT=%8.4f
-\endverbatim
+\endplumedfile
 
 
 */
@@ -90,35 +68,35 @@ DUMPGRID GRID=hB FILE=histoB STRIDE=1 FMT=%8.4f
 namespace PLMD {
 namespace bias {
 
-class ReweightBiasShift : public ReweightBase {
+class ReweightBias : public ReweightBase {
 private:
   double bshift;
 public:
   static void registerKeywords(Keywords&);
-  explicit ReweightBiasShift(const ActionOptions&ao);
+  explicit ReweightBias(const ActionOptions&ao);
   double getLogWeight() const ;
 };
 
-PLUMED_REGISTER_ACTION(ReweightBiasShift,"REWEIGHT_BIAS_SHIFT")
+PLUMED_REGISTER_ACTION(ReweightBias,"REWEIGHT_BIAS")
 
-void ReweightBiasShift::registerKeywords(Keywords& keys ){
+void ReweightBias::registerKeywords(Keywords& keys ) {
   ReweightBase::registerKeywords( keys ); keys.remove("ARG");
   keys.add("compulsory","ARG","*.bias","the biases that must be taken into account when reweighting");
   keys.add("optional","SHIFT","a constant value to shift the bias potential (when they are too large or too small)");
 }
 
-ReweightBiasShift::ReweightBiasShift(const ActionOptions&ao):
-Action(ao),
-ReweightBase(ao),
-bshift(0.0)
+ReweightBias::ReweightBias(const ActionOptions&ao):
+  Action(ao),
+  ReweightBase(ao),
+  bshift(0.0)
 {
-  parse("SHIFT",bshift);
+	parse("SHIFT",bshift);
 }
 
-double ReweightBiasShift::getLogWeight() const {
-   // Retrieve the bias
-   double bias=bshift; for(unsigned i=0;i<getNumberOfArguments();++i) bias+=getArgument(i);
-   return bias / simtemp;
+double ReweightBias::getLogWeight() const {
+  // Retrieve the bias
+  double bias=bshift; for(unsigned i=0; i<getNumberOfArguments(); ++i) bias+=getArgument(i);
+  return bias / simtemp;
 }
 
 }
